f2226ee2f4ca0d79e7fb22a556d354bc96c8426a
reinvantveer <rein.van.t.veer@geodan.nl>
2017-06-15 16:53:23 +0200

    Hopefully fix styling

--- assets/js/datasets.js
+++ assets/js/datasets.js
@@ -8,14 +8,18 @@ $.get('/api/v1/graphs', sets => {
   // Populate void:Dataset part
   sets['@graph']
     .filter(set => set['@type'].find(type => type === 'void:Dataset'))
-    .forEach(dataset => $('#dataset-showcases')
-      .append(`
+    .forEach(dataset => {
+      let uri = dataset['@id'];
+      if (uri[-1] === '/') uri = uri.slice(0, -1);
+      $('#dataset-showcases')
+        .append(`
 <div class="showcase">
   <h2 class="showcase-title">Zonder titel</h2>
-  <a href="${dataset['@id']}">${dataset['@id'].split('/').slice(-1)}</a>
+  <a href="${uri}">${uri.split('/').slice(-1)}</a>
 </div>
 `
-    ));
+        );
+    });
 
   // Populate void:Linkset part
   sets['@graph']
