e321a66e9b953ce53c89195e99c911daf98e5f07
reinvantveer <rein.van.t.veer@geodan.nl>
2017-11-02 11:56:14 +0100

    List latest changes

---
+++ assets/js/latest-updates.js
@@ -0,0 +1,42 @@
+var commitsBaseUrl = "https://api.github.com/repos/PDOK/data.labs.pdok.nl/commits";
+
+var settings = {
+  "async": true,
+  "crossDomain": true,
+  "url": commitsBaseUrl,
+  "method": "GET",
+  "headers": {
+    "accept": "application/vnd.github.v3+json",
+  }
+};
+
+var commits;
+var updatedMarkdownFiles = [];
+
+String.prototype.capitalize = function() {
+  return this.replace(/(?:^|\s)\S/g, function(a) { return a.toUpperCase(); });
+};
+
+$.ajax(settings).done(function (commits) {
+  commits.slice(0,16)
+    .forEach(function (commit) {
+      settings.url = commitsBaseUrl + '/' + commit.sha;
+
+      $.ajax(settings).done(function (response) {
+        response.files
+          .filter(function (file) { return file.filename.slice(-2) === 'md' })
+          .forEach(function (markDownFile) {
+            var path = markDownFile.filename
+              .replace('.md', '.html');
+
+            if ($.inArray(path, updatedMarkdownFiles) === -1) {
+              updatedMarkdownFiles.push(path);
+              $('#updates').append('<a href="/' + path + '">' + path
+                .replace('/index.html', '')
+                .replace('.html', '')
+                .capitalize() + '</a><br />')
+            }
+          });
+    });
+  })
+});
